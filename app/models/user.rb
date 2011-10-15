65142851120ea8a6c1cc59dacbe1fd99869dc4b1
Michael J Kolb <mkolb@meekay.net>
2011-10-15 13:45:42 -0400

    Added mobile key.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   has_and_belongs_to_many :plans
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  validates_uniqueness_of :email
+
+  def initialize (options = {})
+    super
+    self.mobile_key = SecureRandom.hex(16)
+  end
+
+  def generate_mobile_key!
+    update_attribute :mobile_key, SecureRandom.hex(16)
+  end
 end
