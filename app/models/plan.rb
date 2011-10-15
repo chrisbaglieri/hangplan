1f81deff85deaafa64f4f71785ced3ad1e1559dc
Chris Baglieri <chris.baglieri@gmail.com>
2011-10-15 12:11:09 -0400

    wired up user plan association

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,3 +1,3 @@
 class Plan < ActiveRecord::Base
-  
+  belongs_to :owner, :class_name => "User", :foreign_key => :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # Setup accessible (or protected) attributes for your model
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  has_many :plans
   attr_accessible :email, :password, :password_confirmation, :remember_me
 end
