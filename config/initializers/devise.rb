8564fa8f291e5238060d948f6ffc3b597d0854a7
Michael J Kolb <mkolb@meekay.net>
2011-10-15 14:43:16 -0400

    Added Facebook omniauth support.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -197,6 +197,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
+  config.omniauth :facebook, '223798634351683', 'd7978db6783a1636333909168cc5618d'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Hangplan::Application.routes.draw do  
   
-  devise_for :users, :path_names => { :sign_up => "register", :sign_in => "login", :sign_out => "logout"  }
+  devise_for :users, 
+    :path_names => { :sign_up => "register", :sign_in => "login", :sign_out => "logout"  },
+    :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
+    
   devise_scope :user do
     get "/login" => "devise/sessions#new"
     get "/logout" => "devise/sessions#destroy"
+    get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
   end
 
   root :to => "pages#home"
