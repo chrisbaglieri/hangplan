52aa6371fd6434b62d8df4dea5fecf867b4b9bff
Michael J Kolb <mkolb@meekay.net>
2011-10-15 17:12:18 -0400

    Added base model for subscriptions.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, 
     :trackable, :validatable, :omniauthable
   has_and_belongs_to_many :plans
+  has_many :subscriptions
+  has_many :followed_users, :through => :subscriptions
+  has_many :subscribers, :class_name => 'User', :finder_sql => proc { 
+    "SELECT u.* FROM users u INNER JOIN subscriptions s ON s.user_id = u.id WHERE s.followed_user_id = #{id}" }
   attr_accessible :email, :password, :password_confirmation, :remember_me
   validates_uniqueness_of :email
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111015173554) do
+ActiveRecord::Schema.define(:version => 20111015194143) do
 
   create_table "plans", :force => true do |t|
     t.string   "name"
@@ -46,6 +46,16 @@ ActiveRecord::Schema.define(:version => 20111015173554) do
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "subscriptions", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "followed_user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "subscriptions", ["followed_user_id"], :name => "index_subscriptions_on_followed_user_id"
+  add_index "subscriptions", ["user_id"], :name => "index_subscriptions_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "name"
