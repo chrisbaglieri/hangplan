239d181c963f8d4fbfa655883a4f0d005c375131
Chris Baglieri <chris.baglieri@gmail.com>
2011-10-15 13:10:22 -0400

    added create callback and rolled back participants

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,12 @@
 class Plan < ActiveRecord::Base
   belongs_to :owner, :class_name => "User", :foreign_key => :user_id
   attr_accessible :name, :location, :time, :latitude, :longitude, :sponsored, :tentative, :link
-  has_and_belongs_to_many :participants, :class_name => "User", :foreign_key => :user_id
+  has_and_belongs_to_many :users
+  before_create :add_owner_as_participant
+  
+  private
+  
+  def add_owner_as_participant
+    users << owner
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,16 @@ require 'spec_helper'
 describe User do
   it { should validate_presence_of(:email) }
   it { should have_and_belong_to_many(:plans) }
+  
+  describe "owners" do
+    before do
+      @owner = Factory(:user)
+    end
+
+    it "should automatically be added to the plans they own" do
+      Factory(:plan, :owner => @owner)
+      plans = @owner.plans
+      plans.count.should == 1
+    end
+  end
 end
