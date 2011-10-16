032b9bd2b6af7e3d2d7cbb49bd99ebd924c77025
Nathan Vecchiarelli <natevecc@gmail.com>
2011-10-16 12:45:34 -0400

    fixed plans_controller

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,7 +2,7 @@ class PlansController < ApplicationController
   load_and_authorize_resource
   
   def index
-    @plans = Plans.all
+    @plans = Plan.all
     @plans.sort! { |x,y| x.time <=> y.time }
     respond_to do |format|
       format.html
