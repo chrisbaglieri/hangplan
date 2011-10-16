1bb0c5029f606dbe7db9399e2a01e30d51bb9ab9
Michael J Kolb <mkolb@meekay.net>
2011-10-16 10:37:26 -0400

    Added search and subscribe behavior.

--- db/schema.rb
+++ db/schema.rb
@@ -40,14 +40,6 @@ ActiveRecord::Schema.define(:version => 20111016023816) do
 
   add_index "plans", ["user_id"], :name => "index_plans_on_user_id"
 
-  create_table "plans_users", :id => false, :force => true do |t|
-    t.integer "plan_id"
-    t.integer "user_id"
-  end
-
-  add_index "plans_users", ["plan_id"], :name => "plans_users_plan_id"
-  add_index "plans_users", ["user_id"], :name => "plans_users_user_id"
-
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
     t.text     "data"
