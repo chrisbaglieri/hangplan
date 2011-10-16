52caf80cca271e87a8464a826aac8c6862738787
Chris Baglieri <chris.baglieri@gmail.com>
2011-10-16 14:12:03 -0400

    wired up destroy participanbt on plan

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,7 +1,7 @@
 class Plan < ActiveRecord::Base
   belongs_to :owner, :class_name => "User", :foreign_key => :user_id
   attr_accessible :name, :location, :time, :latitude, :longitude, :sponsored, :tentative, :link
-  has_many :participants
+  has_many :participants, :dependent => :destroy
   has_many :users, :through => :participants
   before_create :add_owner_as_participant
   geocoded_by :location
