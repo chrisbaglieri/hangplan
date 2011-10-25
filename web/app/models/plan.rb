527ce796667afce30cb17a26f80fb49394dac37b
Chris Baglieri <chris.baglieri@gmail.com>
2011-10-24 23:20:21 -0400

    cleaned up plan and plan specs

--- web/app/models/plan.rb
+++ web/app/models/plan.rb
@@ -1,19 +1,12 @@
 class Plan < ActiveRecord::Base
   belongs_to :owner, :class_name => "User", :foreign_key => :user_id
-  attr_accessible :name, :location, :time, :latitude, :longitude, :sponsored, :tentative, :link
   has_many :participants, :dependent => :destroy
   has_many :users, :through => :participants
+  attr_accessible :name, :location, :time, :latitude, :longitude, :sponsored, :tentative, :link
   geocoded_by :location
-
-  validates_presence_of :owner
+  validates_presence_of :name, :owner
   after_validation :geocode
   before_create :add_owner_as_participant
-  
-  scope :future, where('time IS NULL OR time > ?', Time.now).order('time asc')
-  
-  def participant(user)
-    Participant.find_by_plan_id_and_user_id(self.id, user)  
-  end
   
   def participant?(user)
     self.users.include?(user)
--- web/spec/models/plan_spec.rb
+++ web/spec/models/plan_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe Plan do
+  before do
+    @user = Factory(:user)
+  end
+  
   it { should belong_to(:owner) }
   it { should have_many(:participants) }
   it { should allow_mass_assignment_of(:name) }
@@ -12,4 +16,13 @@ describe Plan do
   it { should allow_mass_assignment_of(:tentative) }
   it { should allow_mass_assignment_of(:link) }
   it { should have_many(:users) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:owner) }
+  
+  describe "plan owner" do
+    it "should be able to determine if a plan is theirs" do
+      plan = Factory(:plan, :owner => @user)
+      plan.owner?(@user).should == true
+    end
+  end
 end
