976a7766e9d15823d84c10320232eb05929395ae
Michael Kolb <mkolb@meekay.net>
2011-11-03 23:58:39 -0400

    Added specs for time parsing functionality.

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -57,4 +57,68 @@ describe Plan do
       plan.start_at.should >= today
     end
   end
+  
+  it "should parse a valid start date and time" do
+    p = Factory(:plan, :start_date_s => '11/05/2011', :start_time_s => '13:30')
+    p.should_not be_nil
+    p.should be_valid
+    p.should be_persisted
+    p.start_at.should eq(Time.zone.parse('2011-11-05 13:30:00'))
+    p.end_at.should be_nil
+  end
+  
+  it "should work in the end times" do
+    p = Factory(:plan, :start_date_s => '11/05/2011', :start_time_s => '13:30', :end_time_s => '16:30')
+    p.should_not be_nil
+    p.should be_valid
+    p.should be_persisted
+    p.start_at.should eq(Time.zone.parse('2011-11-05 13:30:00'))
+    p.end_at.should eq(Time.zone.parse('2011-11-05 16:30:00'))
+  end
+  
+  it "should handle daylight savings" do
+    Time.use_zone 'America/New_York' do
+      p1 = Factory(:plan, :start_date_s => '11/05/2011', :start_time_s => '13:30', :end_time_s => '16:30')
+      p1.start_at.should eq(Time.zone.parse('2011-11-05, 13:30:00')) # spring ahead
+      p1.start_at.utc_offset.should eq(-4*60*60)
+      p1.end_at.should eq(Time.zone.parse('2011-11-05 16:30:00'))
+      p1.end_at.utc_offset.should eq(p1.start_at.utc_offset)
+      p2 = Factory(:plan, :start_date_s => '11/06/2011', :start_time_s => '13:30', :end_time_s => '16:30')
+      p2.start_at.should eq(Time.zone.parse('2011-11-06, 13:30:00')) # fall behind
+      p2.start_at.utc_offset.should eq(-5*60*60)
+      p2.end_at.should eq(Time.zone.parse('2011-11-06 16:30:00'))
+      p2.end_at.utc_offset.should eq(p2.start_at.utc_offset)
+    end
+  end
+  
+  it "should handle Pacific Standard/Daylight Time" do
+    Time.use_zone 'America/Los_Angeles' do
+      p1 = Factory(:plan, :start_date_s => '11/05/2011', :start_time_s => '13:30', :end_time_s => '16:30')
+      p1.start_at.should eq(Time.zone.parse('2011-11-05, 13:30:00')) # spring ahead
+      p1.start_at.utc_offset.should eq(-7*60*60)
+      p1.end_at.should eq(Time.zone.parse('2011-11-05 16:30:00'))
+      p1.end_at.utc_offset.should eq(p1.start_at.utc_offset)
+      p2 = Factory(:plan, :start_date_s => '11/06/2011', :start_time_s => '13:30', :end_time_s => '16:30')
+      p2.start_at.should eq(Time.zone.parse('2011-11-06, 13:30:00')) # fall behind
+      p2.start_at.utc_offset.should eq(-8*60*60)
+      p2.end_at.should eq(Time.zone.parse('2011-11-06 16:30:00'))
+      p2.end_at.utc_offset.should eq(p2.start_at.utc_offset)
+    end
+  end
+  
+  it "should handle Phoenix Time (no DST)" do
+    Time.use_zone 'America/Phoenix' do
+      p1 = Factory(:plan, :start_date_s => '11/05/2011', :start_time_s => '13:30', :end_time_s => '16:30')
+      p1.start_at.should eq(Time.zone.parse('2011-11-05, 13:30:00')) 
+      p1.start_at.utc_offset.should eq(-7*60*60)
+      p1.end_at.should eq(Time.zone.parse('2011-11-05 16:30:00'))
+      p1.end_at.utc_offset.should eq(p1.start_at.utc_offset)
+      p2 = Factory(:plan, :start_date_s => '11/06/2011', :start_time_s => '13:30', :end_time_s => '16:30')
+      p2.start_at.should eq(Time.zone.parse('2011-11-06, 13:30:00')) # None of that here
+      p2.start_at.utc_offset.should eq(-7*60*60)
+      p2.end_at.should eq(Time.zone.parse('2011-11-06 16:30:00'))
+      p2.end_at.utc_offset.should eq(p2.start_at.utc_offset)
+    end
+  end
+  
 end
