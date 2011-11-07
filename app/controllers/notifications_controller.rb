944c0221f01890af2058ad090d559e424c4402c4
Chris Baglieri <chris.baglieri@gmail.com>
2011-11-07 00:06:16 -0500

    add the raw materials for notification view

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
   def index
     authorize! :read, :notifications
-    @pending_friend_invites = current_user.pending_invited_by
-    @pending_invites = current_user.pending_invited
+    @notifications = []
+    @pending_friend_requests = current_user.pending_invited_by
   end
 end
