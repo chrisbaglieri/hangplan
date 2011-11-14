cce630d47d55e69b3b74199cbb3f8acd1303dc97
Chris Baglieri <chris.baglieri@gmail.com>
2011-11-13 23:05:26 -0500

    added some baseline functionality to facebook for authenticating, loading configs, and bringing up a users profile

--- app/models/identities/facebook.rb
+++ app/models/identities/facebook.rb
@@ -5,4 +5,31 @@ class Facebook < Identity
     super(attributes)
     self.source = "facebook"
   end
+  
+  def profile
+    @profile ||= FbGraph::User.me(self.access_token).fetch
+  end
+  
+  class << self
+    extend ActiveSupport::Memoizable
+
+    def config
+      @config ||= if ENV['fb_client_id'] && ENV['fb_client_secret'] && ENV['fb_scope'] && ENV['fb_canvas_url']
+        {
+          :client_id     => ENV['fb_client_id'],
+          :client_secret => ENV['fb_client_secret'],
+          :scope         => ENV['fb_scope'],
+          :canvas_url    => ENV['fb_canvas_url']
+        }
+      else
+        YAML.load_file("#{Rails.root}/config/facebook.yml")[Rails.env].symbolize_keys
+      end
+    rescue Errno::ENOENT => e
+      raise StandardError.new("config/facebook.yml could not be loaded.")
+    end
+
+    def authenticate(redirect_uri = nil)
+      FbGraph::Auth.new config[:client_id], config[:client_secret], :redirect_uri => redirect_uri
+    end
+  end
 end
